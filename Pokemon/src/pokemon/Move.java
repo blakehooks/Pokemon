30da24967ed46d38edb9542bd31767afd04d20f8
Blake Hooks <bwhooks@ncsu.edu>
2016-01-02 15:07:36 -0500

    Moves must be instantiated as null if no move exists in a slot

--- Pokemon/src/pokemon/Move.java
+++ Pokemon/src/pokemon/Move.java
@@ -6,7 +6,7 @@ public class Move {
 	Type type;
 
 	public Move() {
-		// TODO Auto-generated constructor stub
+		// TODO Auto-generated constructor stub 
 	}
 	
 	public Move(String n, int u, Type t) {
@@ -14,5 +14,38 @@ public class Move {
 		this.uses = u;
 		this.type = t;
 	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		if (name == null) {
+			return "-";
+		}
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the uses
+	 */
+	public int getUses() {
+		return uses;
+	}
+
+	/**
+	 * @param uses the uses to set
+	 */
+	public void setUses(int uses) {
+		this.uses = uses;
+	}
+	
+	
 
 }
