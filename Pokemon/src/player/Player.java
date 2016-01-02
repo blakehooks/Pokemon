58c6c4ae368b7dbb9c8abc7399402379e5b5efa3
Blake Hooks <bwhooks@ncsu.edu>
2016-01-02 13:51:40 -0500

    Adding bag (ItemList) to player

--- Pokemon/src/player/Player.java
+++ Pokemon/src/player/Player.java
@@ -1,25 +1,37 @@
 package player;
 
+import java.util.ArrayList;
+
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+
+import items.Item;
+import items.ItemList;
+import pokemon.Pokemon;
 
 public class Player {
 	private static Player player;
 
+	private ArrayList<Pokemon> party = new ArrayList<Pokemon>();
 	private String name;
 	private Image sprite;
 	private float x,y;
+	private ItemList bag;
 	
 	private Player(float startX, float startY) {
 		this.x = startX;
 		this.y = startY;
+		Pokemon bulb = new Pokemon();
+		party.add(bulb);
 		
 		try {
 			sprite = new Image("assets/img/pikachu.png");
 		} catch (SlickException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		bag = new ItemList();
+		bag.getItems().add(new Item("Potion", 5));
 	}
 	
 	public static Player getPlayer() {
@@ -43,6 +55,14 @@ public class Player {
 	
 	public String getName() {
 		return name;
+	}
+	
+	public Pokemon getFirstInParty() {
+		return party.get(0);
+	}
+	
+	public ItemList getBag() {
+		return bag;
 	}
 
 }
