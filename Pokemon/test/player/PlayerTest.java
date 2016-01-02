ca8442462ca2808a9f93348acc547cf911358dcf
Blake Hooks <bwhooks@ncsu.edu>
2016-01-02 16:55:05 -0500

    Adding test directory

---
+++ Pokemon/test/player/PlayerTest.java
@@ -0,0 +1,20 @@
+package player;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PlayerTest {
+	Player p1;
+	@Before
+	public void setUp() throws Exception {
+		//p1 = Player.getPlayer();
+	}
+
+	@Test
+	public void testGetX() {
+		assertEquals(10, 10);
+	}
+
+}
