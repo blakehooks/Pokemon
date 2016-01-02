c1281a7a5f8a089ca2841648af7639f30d3cbeb9
Blake Hooks <bwhooks@ncsu.edu>
2016-01-02 13:51:16 -0500

    Adding parameter based construtor
    Allows you to specify name, hp and xy coordinates of the image in a
    spritesheet

--- Pokemon/src/pokemon/Pokemon.java
+++ Pokemon/src/pokemon/Pokemon.java
@@ -1,9 +1,74 @@
 package pokemon;
 
-public class Pokemon {
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.SpriteSheet;
 
+public class Pokemon {
+	private String name;
+	private Image sprite;
+	private int hp;
+	
 	public Pokemon() {
-		// TODO Auto-generated constructor stub
+		name = "Bulbasaur";
+		SpriteSheet ss = null;
+		try {
+			ss = new SpriteSheet(new Image("assets/img/sprites2.png"), 41, 41);
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
+		sprite = ss.getSprite(0, 0);
+		hp = 100;
 	}
+	
+	public Pokemon(String n, int x, int y, int hp) {
+		this.name = n;
+		SpriteSheet ss = null;
+		try {
+			ss = new SpriteSheet(new Image("assets/img/sprites2.png"), 41, 41);
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
+		this.sprite = ss.getSprite(x, y);
+		this.hp = hp;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the sprite
+	 */
+	public Image getSprite() {
+		return sprite;
+	}
+	/**
+	 * @param sprite the sprite to set
+	 */
+	public void setSprite(Image sprite) {
+		this.sprite = sprite;
+	}
+	/**
+	 * @return the hp
+	 */
+	public int getHp() {
+		return hp;
+	}
+	/**
+	 * @param hp the hp to set
+	 */
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+	
+	
 
 }
